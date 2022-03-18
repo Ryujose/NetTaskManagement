7cb8490bb0a99c9d0908a0ebf195084f371ba3de
Jose Luis Guerra Infante <sora_ryu@hotmail.com>
2022-03-18 02:38:29 +0100

    InitialCompleteFunctionality: add all

---
+++ src/NetFramework.Tasks.Management.xUnit.Tests/TaskManagementCheckAllTaskStatusCompletedTest.cs
@@ -0,0 +1,71 @@
+﻿using NetFramework.Tasks.Management;
+using NetFramework.Tasks.Management.Abstractions.Enums;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace NetFramework.Tasks.Management.Tests
+{
+    public class TaskManagementCheckAllTaskStatusCompletedTest
+    {
+        [Fact]
+        public void CheckAllTaskStatusCompletedTasksCompleted_TaskManagementCheckAllTaskStatusCompleted_AllTasksCompletedStatus()
+        {
+            TasksManagement _taskManagement = new TasksManagement();
+
+            const string simpleTaskName = "tasktest1";
+            const string simpleTaskNameTwo = "tasktest2";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+
+            var cancellationTokenSource2 = new CancellationTokenSource();
+            taskManagementStatus = _taskManagement.RegisterTask(simpleTaskNameTwo, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource2);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskNameTwo);
+
+            var tasksCancelPetitionFailedRef = new ConcurrentDictionary<string, TaskManagementStatus>();
+            taskManagementStatus = _taskManagement.CancelAllTasks(null, ref tasksCancelPetitionFailedRef);
+
+            var tasksCompletedStatusRef = new ConcurrentDictionary<string, TaskManagementStatus>();
+            _taskManagement.CheckAllTaskStatusCompleted(null, ref tasksCompletedStatusRef, millisecondsCancellationWait: 10000);
+
+            bool result = tasksCompletedStatusRef.Any(a => a.Value != TaskManagementStatus.Completed);
+
+            Assert.False(result);
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void CheckAllTaskStatusCompletedTasksCompletedExcepted_TaskManagementCheckAllTaskStatusCompleted_AllTasksCompletedStatus()
+        {
+            TasksManagement _taskManagement = new TasksManagement();
+
+            const string simpleTaskName = "tasktest1";
+            const string simpleTaskNameTwo = "tasktest2";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+
+            var cancellationTokenSource2 = new CancellationTokenSource();
+            taskManagementStatus = _taskManagement.RegisterTask(simpleTaskNameTwo, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource2);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+
+            var tasksCancelPetitionFailedRef = new ConcurrentDictionary<string, TaskManagementStatus>();
+            taskManagementStatus = _taskManagement.CancelAllTasks(new List<string> { simpleTaskName }, ref tasksCancelPetitionFailedRef);
+
+            var tasksCompletedStatusRef = new ConcurrentDictionary<string, TaskManagementStatus>();
+            _taskManagement.CheckAllTaskStatusCompleted(new List<string> { simpleTaskName }, ref tasksCompletedStatusRef);
+
+            bool result = tasksCompletedStatusRef.Any(a => a.Value != TaskManagementStatus.Completed);
+            taskManagementStatus = _taskManagement.CheckTaskStatusCompleted(simpleTaskName, retry: 0, millisecondsCancellationWait: 1);
+
+            Assert.Equal(TaskManagementStatus.NotCompleted, taskManagementStatus);
+            Assert.False(result);
+            _taskManagement.ClearConcurrentLists();
+        }
+    }
+}
---
+++ src/NetFramework.Tasks.Management.xUnit.Tests/TaskManagementDeleteTaskTest.cs
@@ -0,0 +1,123 @@
+﻿using NetFramework.Tasks.Management;
+using NetFramework.Tasks.Management.Abstractions.Enums;
+using NetFramework.Tasks.Management.Abstractions.Models;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace NetFramework.Tasks.Management.Tests
+{
+    public class TaskManagementDeleteTaskTest 
+    {
+        TasksManagement _taskManagement;
+
+        public TaskManagementDeleteTaskTest()
+        {
+            _taskManagement = new TasksManagement();
+        }
+
+        [Fact]
+        public void OnlyRegisteredDeleteTask_TaskManagementRegister_InvalidOperationException()
+        {
+            const string simpleTaskName = "tasktest";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+
+            Assert.Throws<InvalidOperationException>(() => _taskManagement.DeleteTask(simpleTaskName));
+
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void NotRegisteredDeleteTask_TaskManagementRegister_TaskNotFoundTaskStatus()
+        {
+            const string simpleTaskName = "tasktest";
+
+            TaskManagementStatus taskManagementStatus = _taskManagement.DeleteTask(simpleTaskName);
+
+            Assert.Equal(TaskManagementStatus.TaskNotFound, taskManagementStatus);
+
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void StartedDeleteTask_TaskManagementRegister_InvalidOperationException()
+        {
+            const string simpleTaskName = "tasktest";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            Assert.Throws<InvalidOperationException>(() => _taskManagement.DeleteTask(simpleTaskName));
+
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void StartedWaitedCompletionDeleteTask_TaskManagementRegister_StartedTaskStatus()
+        {
+            const string simpleTaskName = "tasktest";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CancelTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CheckTaskStatusCompleted(simpleTaskName, retry: 1, millisecondsCancellationWait: 100);
+            taskManagementStatus = _taskManagement.DeleteTask(simpleTaskName);
+
+            Assert.Equal(TaskManagementStatus.Deleted, taskManagementStatus);
+
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void StartedWaitedCancellationTokenSourceNullCompletionDeleteTask_TaskManagementRegister_StartedTaskStatus()
+        {
+            const string simpleTaskName = "tasktest";
+
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), null);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+
+            Assert.Equal(TaskManagementStatus.TaskNotFound, taskManagementStatus);
+
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void StartedWaitedCompletionDeleteTaskQueueActivated_TaskManagementRegister_StartedTaskStatus()
+        {
+            const string simpleTaskName = "tasktest";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CancelTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CheckTaskStatusCompleted(simpleTaskName, retry: 1, millisecondsCancellationWait: 1000);
+            taskManagementStatus = _taskManagement.DeleteTask(simpleTaskName, sendDataToInternalQueue: true);
+            taskManagementStatus = _taskManagement.DequeueTaskDisposedDataModel(out TaskDisposedDataModel taskDisposedDataModel);
+
+            Assert.Equal(TaskManagementStatus.ObjectInfoDequeued, taskManagementStatus);
+            
+            _taskManagement.ClearConcurrentLists();
+        }
+
+        [Fact]
+        public void StartedWaitedCompletionDeleteTaskQueueDeactivated_TaskManagementRegister_StartedTaskStatus()
+        {
+            const string simpleTaskName = "tasktest";
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            TaskManagementStatus taskManagementStatus = _taskManagement.RegisterTask(simpleTaskName, new ActionsUtilitiesTests().ActionObjectCancellationTokenSource(), cancellationTokenSource);
+            taskManagementStatus = _taskManagement.StartTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CancelTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.CheckTaskStatusCompleted(simpleTaskName, retry: 1, millisecondsCancellationWait: 1000);
+            taskManagementStatus = _taskManagement.DeleteTask(simpleTaskName);
+            taskManagementStatus = _taskManagement.DequeueTaskDisposedDataModel(out TaskDisposedDataModel taskDisposedDataModel);
+
+            Assert.Equal(TaskManagementStatus.ObjectInfoNotDequeuedOrFound, taskManagementStatus);
+
+            _taskManagement.ClearConcurrentLists();
+        }
+    }
+}
