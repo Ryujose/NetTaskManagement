ded126a76ec87f31c3b4df79b135d07fe9ff2a97
Jose Luis Guerra Infante <sora_ryu@hotmail.com>
2022-03-27 03:13:50 +0200

    AddMicrosoftLogs: <-

---
+++ src/NetFramework.Task.Management/AddTaskManagement.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NetFramework.Tasks.Management.Abstractions.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetFramework.Tasks.Management
+{
+    public static class TaskMangementDIRegisterExtension
+    {
+        public static void AddTaskManagement(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton(typeof(ITaskManagement),
+                (serviceProvider) =>
+                {
+                    return new TasksManagement(serviceProvider.GetService<ILogger>());
+                });
+        }
+    }
+}
--- src/NetFramework.Tasks.Management.xUnit.Tests/TaskManagementStartTest.cs
+++ src/NetFramework.Tasks.Management.xUnit.Tests/TaskManagementStartTest.cs
@@ -1,4 +1,6 @@
-﻿using NetFramework.Tasks.Management;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using NetFramework.Tasks.Management;
 using NetFramework.Tasks.Management.Abstractions.Enums;
 using System.Threading;
 using Xunit;
@@ -11,7 +13,8 @@ namespace NetFramework.Tasks.Management.Tests
 
         public TaskManagementStartTest()
         {
-            _taskManagement = new TasksManagement();
+            var logger = new Mock<ILogger>();
+            _taskManagement = new TasksManagement(logger.Object);
         }
 
         [Fact]
